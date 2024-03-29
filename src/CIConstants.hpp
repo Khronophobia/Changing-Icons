9a4b2b72291b44f8245b5c8015af157f3f0c33f1
Khronophobia <46954143+Khronophobia@users.noreply.github.com>
2024-03-29 14:41:45 +0700

    Make icon cell a bit bigger

--- src/CIConstants.hpp
+++ src/CIConstants.hpp
@@ -3,7 +3,7 @@
 // Idk if there's a function to get the color count
 namespace changing_icons::constants {
     inline float constexpr ICONCELL_WIDTH = 160.f;
-    inline float constexpr ICONCELL_HEIGHT = 30.f;
+    inline float constexpr ICONCELL_HEIGHT = 36.f;
     inline float constexpr PRESETCELL_WIDTH = 220.f;
     inline float constexpr PRESETCELL_HEIGHT = 44.f;
     inline int constexpr COLOR_COUNT = 106;
