679ae7802c847b0b5c180bccb3b5e195e9640eac
Khronophobia <46954143+Khronophobia@users.noreply.github.com>
2024-04-23 13:42:42 +0700

    Update geode version

--- src/hooks/PlayerObject.hpp
+++ src/hooks/PlayerObject.hpp
@@ -3,18 +3,20 @@
 #include <properties/TempProperties.hpp>
 
 class $modify(CIPlayerObject, PlayerObject) {
-    bool m_levelStarted;
-    std::unordered_map<IconType, changing_icons::CITempProperties> m_ciProperties;
-    inline static changing_icons::CITempProperties s_emptyCIProperty = changing_icons::CITempProperties();
-    int m_ogColor1 = GameManager::get()->getPlayerColor();
-    int m_ogColor2 = GameManager::get()->getPlayerColor2();
-    int m_ogGlowColor = GameManager::get()->getPlayerGlowColor();
-    bool m_ogHasGlow;
-    cocos2d::ccColor3B m_currentColor;
-    cocos2d::ccColor3B m_currentVehicleColor;
-    cocos2d::ccColor3B m_vehicleGlowColor;
-    bool m_hasVehicleGlow;
-    bool m_hasCustomVehicleGlowColor;
+    struct Fields {
+        bool m_levelStarted;
+        std::unordered_map<IconType, changing_icons::CITempProperties> m_ciProperties;
+        inline static changing_icons::CITempProperties s_emptyCIProperty = changing_icons::CITempProperties();
+        int m_ogColor1 = GameManager::get()->getPlayerColor();
+        int m_ogColor2 = GameManager::get()->getPlayerColor2();
+        int m_ogGlowColor = GameManager::get()->getPlayerGlowColor();
+        bool m_ogHasGlow;
+        cocos2d::ccColor3B m_currentColor;
+        cocos2d::ccColor3B m_currentVehicleColor;
+        cocos2d::ccColor3B m_vehicleGlowColor;
+        bool m_hasVehicleGlow;
+        bool m_hasCustomVehicleGlowColor;
+    };
 
     $override
     bool init(int p0, int p1, GJBaseGameLayer* p2, cocos2d::CCLayer* p3, bool p4);
